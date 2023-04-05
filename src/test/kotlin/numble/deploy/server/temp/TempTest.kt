c877d1e084ae3b3f23871fafa5075ec6cc41d588
ggam-nyang <v44ads@naver.com>
2023-04-05 21:59:51 +0900

    test for jenkins

---
+++ src/test/kotlin/numble/deploy/server/temp/TempTest.kt
@@ -0,0 +1,16 @@
+package numble.deploy.server.temp
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+private class TempTest {
+
+    @Test
+    fun temp() {
+        val a = 1
+        val b = a
+
+        println("test run")
+        Assertions.assertThat(a).isEqualTo(b)
+    }
+}
