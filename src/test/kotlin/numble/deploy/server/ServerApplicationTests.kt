17cfa3419f7b324b6d8f5c9f9d0348c18e24c4e0
ggam-nyang <v44ads@naver.com>
2023-04-05 19:52:04 +0900

    git init && simple spring boot project init

---
+++ src/test/kotlin/numble/deploy/server/ServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package numble.deploy.server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
